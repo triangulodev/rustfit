579f2c963167457c5ec2b2e0fdf8e4441bee3984
Hugo de Oliveira <hugo@triangulo.dev>
2023-10-24 13:09:49 -0300

    Add basic accounts post endpoint

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,32 @@ use axum::{
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 
+#[derive(Deserialize)]
+struct CreateAccount {
+    email: String,
+}
+
+#[derive(Serialize)]
+struct Account {
+    id: u64,
+    email: String,
+}
+
+async fn create_account(Json(payload): Json<CreateAccount>) -> impl IntoResponse {
+    let account = Account {
+        id: 1337,
+        email: payload.email,
+    };
+
+    (StatusCode::CREATED, Json(account))
+}
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
-    let app = Router::new().route("/", get(root));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/api/v1/accounts", post(create_account));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::info!("listening on {}", addr);

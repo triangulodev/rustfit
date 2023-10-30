04d35d0f4bbd19d24559f47fe265fa78fd750c1b
Hugo de Oliveira <hugo@triangulo.dev>
2023-10-30 17:46:32 -0300

    Add login account with random token

--- src/http/accounts.rs
+++ src/http/accounts.rs
@@ -1,12 +1,16 @@
 use crate::http::{ApiContext, Result};
 use crate::models::account::AccountDTO;
+use crate::models::account::AccountWithTokenDTO;
+use crate::models::account::LoginCredentials;
 use crate::models::account::NewAccount;
 use axum::extract::State;
 use axum::routing::post;
 use axum::{Json, Router};
 
 pub(crate) fn router() -> Router<ApiContext> {
-    Router::new().route("/api/accounts", post(create_account))
+    Router::new()
+        .route("/api/accounts", post(create_account))
+        .route("/api/login", post(login_account))
 }
 
 #[derive(serde::Serialize, serde::Deserialize)]
@@ -19,6 +23,15 @@ async fn create_account(
     Json(req): Json<AccountBody<NewAccount>>,
 ) -> Result<Json<AccountBody<AccountDTO>>> {
     let account = ctx.store.account().create_account(req.account).await?;
+
+    Ok(Json(AccountBody { account }))
+}
+
+async fn login_account(
+    ctx: State<ApiContext>,
+    Json(credentials): Json<LoginCredentials>,
+) -> Result<Json<AccountBody<AccountWithTokenDTO>>> {
+    let account = ctx.store.account().login_account(credentials).await?;
 
     Ok(Json(AccountBody { account }))
 }
--- src/models/account.rs
+++ src/models/account.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use crate::http::Result;
+use crate::http::{Error, Result};
 use anyhow::Context;
 use argon2::{password_hash::SaltString, Argon2, PasswordHash};
 use async_trait::async_trait;
@@ -12,6 +12,12 @@ use uuid::Uuid;
 #[derive(serde::Deserialize)]
 pub struct NewAccount {
     pub name: String,
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(serde::Deserialize)]
+pub struct LoginCredentials {
     pub email: String,
     pub password: String,
 }
@@ -35,6 +41,26 @@ pub struct AccountDTO {
     pub updated_at: OffsetDateTime,
 }
 
+#[derive(serde::Serialize, serde::Deserialize, sqlx::FromRow)]
+pub struct AccountWithTokenDTO {
+    pub id: Uuid,
+    pub email: String,
+    pub name: String,
+    pub token: String,
+    pub inserted_at: OffsetDateTime,
+    pub updated_at: OffsetDateTime,
+}
+
+#[derive(serde::Serialize, serde::Deserialize, sqlx::FromRow)]
+pub struct AccountWithPasswordHashDTO {
+    pub id: Uuid,
+    pub email: String,
+    pub name: String,
+    pub password_hash: String,
+    pub inserted_at: OffsetDateTime,
+    pub updated_at: OffsetDateTime,
+}
+
 #[derive(Clone)]
 pub struct AccountController {
     pool: SqlitePool,
@@ -50,6 +76,9 @@ pub type DynAccountCtrl = Arc<dyn AccountCtrlTrait + Send + Sync>;
 #[async_trait]
 pub trait AccountCtrlTrait {
     async fn create_account(&self, new_account: NewAccount) -> Result<AccountDTO>;
+    async fn login_account(&self, credentials: LoginCredentials) -> Result<AccountWithTokenDTO>;
+
+    async fn get_account_by_email(&self, email: String) -> Result<AccountWithPasswordHashDTO>;
 }
 
 #[async_trait]
@@ -82,6 +111,41 @@ impl AccountCtrlTrait for AccountController {
 
         Ok(account)
     }
+
+    async fn login_account(&self, login_account: LoginCredentials) -> Result<AccountWithTokenDTO> {
+        let account = &self.get_account_by_email(login_account.email).await?;
+
+        Account::verify_password(login_account.password, account.password_hash.clone()).await?;
+
+        // FIXME: token is not supposed to be a random string, should actually be safe and attached
+        // to a session.
+        let token = rand::random::<u32>().to_string();
+        // create token
+        // merge token to account
+
+        Ok(AccountWithTokenDTO {
+            id: account.id,
+            name: account.name.clone(),
+            email: account.email.clone(),
+            token,
+            inserted_at: account.inserted_at,
+            updated_at: account.updated_at,
+        })
+    }
+
+    async fn get_account_by_email(&self, email: String) -> Result<AccountWithPasswordHashDTO> {
+        let account = sqlx::query_as!(
+            AccountWithPasswordHashDTO,
+            r#"select
+                id as "id: Uuid", name, email, password_hash,
+                inserted_at as "inserted_at: OffsetDateTime", updated_at as "updated_at: OffsetDateTime"
+            from accounts
+            where email = $1"#,
+                  email
+            ).fetch_one(&self.pool).await?;
+
+        Ok(account)
+    }
 }
 
 impl Account {
@@ -96,5 +160,20 @@ impl Account {
         })
         .await
         .context("panic in generating password hash")?
+    }
+
+    pub(crate) async fn verify_password(password: String, password_hash: String) -> Result<()> {
+        tokio::task::spawn_blocking(move || -> Result<()> {
+            let hash = PasswordHash::new(&password_hash)
+                .map_err(|e| anyhow::anyhow!("invalid password hash: {}", e))?;
+
+            hash.verify_password(&[&Argon2::default()], password)
+                .map_err(|e| match e {
+                    argon2::password_hash::Error::Password => Error::Unauthorized,
+                    _ => anyhow::anyhow!("failed to verify password hash: {}", e).into(),
+                })
+        })
+        .await
+        .context("panic in verifying password hash")?
     }
 }
